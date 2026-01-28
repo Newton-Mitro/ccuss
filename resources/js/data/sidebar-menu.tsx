e0afa14052b3a6c4bad597f4d9a218dcb301991a
newton-mitro <newtonmitro@gmail.com>
2026-01-28 17:23:13 +0600

    misc

--- resources/js/data/sidebar-menu.tsx
+++ resources/js/data/sidebar-menu.tsx
@@ -4,18 +4,30 @@ export const sidebarMenu: SidebarItem[] = [
     {
         name: 'Dashboard',
         icon: <i className="fa-solid fa-gauge-high" />,
-        path: '/dashboard',
-        match_path: 'dashboard',
+        children: [
+            {
+                name: 'User Dashboard',
+                icon: <i className="fa-solid fa-gauge-high" />,
+                path: '/dashboard',
+                match_path: 'dashboard',
+            },
+            {
+                name: 'Admin Dashboard',
+                icon: <i className="fa-solid fa-chart-pie"></i>,
+                path: '/admin-dashboard',
+                match_path: 'admin-dashboard',
+            },
+        ],
     },
     {
-        name: 'All Media',
+        name: 'Media and Files',
         icon: <i className="fa-solid fa-photo-film"></i>,
         path: '/auth/media',
         match_path: 'media',
     },
     {
         name: 'Organizational Structure',
-        icon: <i className="fa-solid fa-photo-film"></i>,
+        icon: <i className="fa-solid fa-building-flag"></i>,
         children: [
             {
                 name: 'Branches',
@@ -84,15 +96,15 @@ export const sidebarMenu: SidebarItem[] = [
                 match_path: 'gl_accounts',
             },
             {
-                name: 'Manage Vouchers',
+                name: 'Vouchers',
+                icon: <i className="fa-solid fa-coins" />,
+                path: '/auth/vouchers/list',
+                match_path: 'vouchers/list',
+            },
+            {
+                name: 'Voucher Entrys',
                 icon: <i className="fa-solid fa-cash-register" />,
                 children: [
-                    {
-                        name: 'Vouchers',
-                        icon: <i className="fa-solid fa-coins" />,
-                        path: '/auth/vouchers/list',
-                        match_path: 'vouchers/list',
-                    },
                     {
                         name: 'Debit Vouchers',
                         icon: <i className="fa-solid fa-coins" />,
@@ -243,53 +255,41 @@ export const sidebarMenu: SidebarItem[] = [
     },
     {
         name: 'Loan Product Mgmt.',
-        icon: <i className="fa-solid fa-sprout" />,
+        icon: <i className="fa-solid fa-sack-dollar"></i>,
         children: [
             {
                 name: 'Loan Products',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-policies',
                 match_path: 'deposit-policies',
             },
             {
-                name: 'Tenure Wise Interest Rates',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                name: 'Interest Policies',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-tenures',
                 match_path: 'deposit-tenures',
             },
             {
-                name: 'Opening Policies',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                name: 'Fine Policies',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/saving-accounts',
                 match_path: 'saving-accounts',
             },
             {
-                name: 'Transaction Policies',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                name: 'Approval Policies',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-holders',
                 match_path: 'deposit-holders',
             },
             {
-                name: 'Interest Policies',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                name: 'Charge Policies',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-nominees',
                 match_path: 'deposit-nominees',
             },
             {
-                name: 'Closing Policies',
-                icon: <i className="fa-solid fa-piggy-bank" />,
-                path: '/deposit-schedules',
-                match_path: 'deposit-schedules',
-            },
-            {
-                name: 'Fine Policies',
-                icon: <i className="fa-solid fa-piggy-bank" />,
-                path: '/deposit-schedules',
-                match_path: 'deposit-schedules',
-            },
-            {
-                name: 'Product Ldeger Mappings',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                name: 'Repayment Policies',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-schedules',
                 match_path: 'deposit-schedules',
             },
@@ -297,23 +297,23 @@ export const sidebarMenu: SidebarItem[] = [
     },
     {
         name: 'Loan Account Mgmt.',
-        icon: <i className="fa-solid fa-sprout" />,
+        icon: <i className="fa-solid fa-sack-dollar"></i>,
         children: [
             {
                 name: 'Loan Accounts',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-policies',
                 match_path: 'deposit-policies',
             },
             {
                 name: 'Loan Schedules',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-tenures',
                 match_path: 'deposit-tenures',
             },
             {
                 name: 'Loan Collaters',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/saving-accounts',
                 match_path: 'saving-accounts',
             },
@@ -321,53 +321,53 @@ export const sidebarMenu: SidebarItem[] = [
     },
     {
         name: 'Loan Application Mgmt.',
-        icon: <i className="fa-solid fa-sprout" />,
+        icon: <i className="fa-solid fa-sack-dollar"></i>,
         children: [
             {
                 name: 'Loan Applications',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-policies',
                 match_path: 'deposit-policies',
             },
             {
                 name: 'Surity Requests',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/deposit-tenures',
                 match_path: 'deposit-tenures',
             },
             {
                 name: 'Collaterals',
-                icon: <i className="fa-solid fa-piggy-bank" />,
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
                 path: '/saving-accounts',
                 match_path: 'saving-accounts',
             },
         ],
     },
     {
-        name: 'Cash Mgmt.',
-        icon: <i className="fa-solid fa-user-tie" />,
+        name: 'Cash & Treasury Mgmt.',
+        icon: <i className="fa-solid fa-cash-register"></i>,
         children: [
             {
                 name: 'Cash at Hand',
-                icon: <i className="fa-solid fa-user-tie" />,
+                icon: <i className="fa-solid fa-hand-holding-dollar"></i>,
                 path: '/employees',
                 match_path: 'employees',
             },
             {
                 name: 'Petty Cash',
-                icon: <i className="fa-solid fa-user-tie" />,
+                icon: <i className="fa-solid fa-coins"></i>,
                 path: '/attendances',
                 match_path: 'attendances',
             },
             {
                 name: 'Cash at Bank',
-                icon: <i className="fa-solid fa-user-tie" />,
+                icon: <i className="fa-solid fa-piggy-bank"></i>,
                 path: '/leaves',
                 match_path: 'leaves',
             },
             {
                 name: 'Bank Cheque Books',
-                icon: <i className="fa-solid fa-landmark" />,
+                icon: <i className="fa-solid fa-money-check-dollar"></i>,
                 path: '/cheque-book',
                 match_path: 'cheque-book',
             },
@@ -395,22 +395,95 @@ export const sidebarMenu: SidebarItem[] = [
                 path: '/attendances',
                 match_path: 'attendances',
             },
+            {
+                name: 'Holidays',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/holidays',
+                match_path: 'holidays',
+            },
+            {
+                name: 'Shifts',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/shifts',
+                match_path: 'shifts',
+            },
+            {
+                name: 'Overtime',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/overtimes',
+                match_path: 'overtimes',
+            },
+            {
+                name: 'Deductions',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/deductions',
+                match_path: 'deductions',
+            },
+            {
+                name: 'Payslips',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payslips',
+                match_path: 'payslips',
+            },
+            {
+                name: 'Payroll',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payroll',
+                match_path: 'payroll',
+            },
+            {
+                name: 'Payroll Items',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payroll-items',
+                match_path: 'payroll-items',
+            },
+            {
+                name: 'Payroll Templates',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payroll-templates',
+                match_path: 'payroll-templates',
+            },
+            {
+                name: 'Payroll Settings',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payroll-settings',
+                match_path: 'payroll-settings',
+            },
+            {
+                name: 'Payroll Reports',
+                icon: <i className="fa-solid fa-user-tie" />,
+                path: '/payroll-reports',
+                match_path: 'payroll-reports',
+            },
+        ],
+    },
+
+    {
+        name: 'Procurement Mgmt.',
+        icon: <i className="fa-solid fa-sack-dollar"></i>,
+        children: [
+            {
+                name: 'Loan Accounts',
+                icon: <i className="fa-solid fa-sack-dollar"></i>,
+                path: '/deposit-policies',
+                match_path: 'deposit-policies',
+            },
+            {
+                name: 'Fixed Assets Mgmt.',
+                icon: <i className="fa-solid fa-user-gear" />,
+                children: [
+                    {
+                        name: 'Asset Categories',
+                        icon: <i className="fa-solid fa-user-gear" />,
+                        path: '/users',
+                        match_path: 'users',
+                    },
+                ],
+            },
         ],
     },
     {
-        name: 'Fixed Assets',
-        icon: <i className="fa-solid fa-user-gear" />,
-        path: '/users/roles',
-        match_path: 'roles',
-    },
-    {
-        name: 'Vendors/Suppliers',
-        icon: <i className="fa-solid fa-user-gear" />,
-        path: '/users/roles',
-        match_path: 'roles',
-    },
-    {
-        name: 'User Auth',
+        name: 'User Auth Mgmt.',
         icon: <i className="fa-solid fa-user-gear" />,
         children: [
             {
@@ -448,13 +521,43 @@ export const sidebarMenu: SidebarItem[] = [
     {
         name: 'Audit Trails',
         icon: <i className="fa-solid fa-user-gear" />,
-        path: '/users/roles',
-        match_path: 'roles',
+        children: [
+            {
+                name: 'Audit Logs',
+                icon: <i className="fa-solid fa-user-gear" />,
+                path: '/users/user-permissions',
+                match_path: 'user-permissions',
+            },
+            {
+                name: 'Activity Logs',
+                icon: <i className="fa-solid fa-user-gear" />,
+                path: '/users/user-permissions',
+                match_path: 'user-permissions',
+            },
+            {
+                name: 'Visits Logs',
+                icon: <i className="fa-solid fa-user-gear" />,
+                path: '/users/user-permissions',
+                match_path: 'user-permissions',
+            },
+        ],
     },
     {
         name: 'User Settings',
         icon: <i className="fa-solid fa-gear" />,
-        path: '/settings/profile',
-        match_path: 'settings',
+        children: [
+            {
+                name: 'User Settings',
+                icon: <i className="fa-solid fa-gear" />,
+                path: '/settings/profile',
+                match_path: 'settings',
+            },
+            {
+                name: 'Application Settings',
+                icon: <i className="fa-solid fa-gear" />,
+                path: '/settings/application',
+                match_path: 'application',
+            },
+        ],
     },
 ];
